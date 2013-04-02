1375c63d7d16429f0653f9979bc7ec0f9f3ef8b9
resteve <resteve@zikzakmedia.com>
2013-04-02 19:55:33 +0200

    Header py files

--- account.py
+++ account.py
@@ -1,5 +1,6 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+#This file is part account_parent_code module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL
 from itertools import izip
 
