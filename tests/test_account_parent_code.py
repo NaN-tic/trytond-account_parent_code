0aea1d1a99e6e29f58fff7bc2a2cf5c318ef840a
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-08-01 11:28:19 +0200

    Fix and cleanup tests

--- tests/test_account_parent_code.py
+++ tests/test_account_parent_code.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
@@ -110,6 +102,7 @@ class AccountParentCodeTestCase(unittest.TestCase):
             self.account.delete([account_1])
             self.assertEqual(self.account.search([]), [])
 
+
 def suite():
     suite = trytond.tests.test_tryton.suite()
     from trytond.modules.company.tests import test_company
@@ -119,6 +112,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountParentCodeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
