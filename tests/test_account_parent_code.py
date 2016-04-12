996a581fea52be7f5cfa2bcae046f372970cd27e
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-12 10:57:55 +0200

    Update tests

--- tests/test_account_parent_code.py
+++ tests/test_account_parent_code.py
@@ -2,104 +2,92 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+from trytond.pool import Pool
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
+
+from trytond.modules.company.tests import create_company, set_company
 
 
 class AccountParentCodeTestCase(ModuleTestCase):
     'Test Account Parent Code module'
     module = 'account_parent_code'
 
-    def setUp(self):
-        super(AccountParentCodeTestCase, self).setUp()
-        self.account_template = POOL.get('account.account.template')
-        self.account = POOL.get('account.account')
-        self.company = POOL.get('company.company')
-        self.user = POOL.get('res.user')
-        self.fiscalyear = POOL.get('account.fiscalyear')
-        self.sequence = POOL.get('ir.sequence')
-
+    @with_transaction()
     def test0010parent_code(self):
         'Test parent code'
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT):
-            company, = self.company.search([
-                    ('party.name', '=', 'Dunder Mifflin')])
-            self.user.write([self.user(USER)], {
-                    'main_company': company.id,
-                    'company': company.id,
-                    })
-            root, = self.account.create([{
+        pool = Pool()
+        Account = pool.get('account.account')
+
+        company = create_company()
+        with set_company(company):
+            root, = Account.create([{
                         'name': 'root',
                         'code': '',
                         'kind': 'view',
                         'company': company.id,
                         }])
-            account_1, = self.account.create([{
+            account_1, = Account.create([{
                         'name': 'root',
                         'code': '1',
                         'kind': 'view',
                         'company': company.id,
                         }])
-            account_100, = self.account.create([{
+            account_100, = Account.create([{
                         'name': 'root',
                         'code': '100',
                         'kind': 'view',
                         'company': company.id,
                         }])
-            account_10, = self.account.create([{
+            account_10, = Account.create([{
                         'name': 'root',
                         'code': '10',
                         'kind': 'view',
                         'company': company.id,
                         }])
-            account_2, = self.account.create([{
+            account_2, = Account.create([{
                         'name': 'root',
                         'code': '2',
                         'kind': 'view',
                         'company': company.id,
                         }])
 
-            account, = self.account.search([('code', '=', '2')])
+            account, = Account.search([('code', '=', '2')])
             self.assertEqual(account, account_2)
             self.assertEqual(account.parent, root)
 
-            account, = self.account.search([('code', '=', '100')])
+            account, = Account.search([('code', '=', '100')])
             self.assertEqual(account, account_100)
             self.assertEqual(account.parent, account_10)
             self.assertEqual(account.parent.parent, account_1)
             self.assertEqual(account.parent.parent.parent, root)
 
-            self.account.delete([account_10])
+            Account.delete([account_10])
             self.assertEqual(account_100.parent, account_1)
 
-            self.account.write([account_1], {
+            Account.write([account_1], {
                     'code': '20'
                     })
             self.assertEqual(account_100.parent, root)
             self.assertEqual(account_1.parent, account_2)
 
-            self.account.delete([account_2])
+            Account.delete([account_2])
             self.assertEqual(account_1.parent, root)
-            self.account.delete([root])
+            Account.delete([root])
             self.assertEqual(account_1.parent, None)
             self.assertEqual(account_100.parent, None)
-            self.account.write([account_1], {
+            Account.write([account_1], {
                     'code': '1',
                     })
             self.assertEqual(account_100.parent, account_1)
-            self.account.delete([account_100])
+            Account.delete([account_100])
             self.assertEqual(account_1.childs, ())
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite:
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountParentCodeTestCase))
     return suite
