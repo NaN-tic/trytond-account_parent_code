4d96dca965f0d7327ffeaaa3cefacb9a74016bab
Raimon Esteve <raimon@nan-tic.com>
2016-05-26 13:43:39 +0200

    DOC

--- __init__.py
+++ __init__.py
@@ -1,10 +1,8 @@
 # This file is part account_parent_code module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import Pool
 from .account import *
-
 
 def register():
     Pool.register(
